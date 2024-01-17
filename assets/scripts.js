3b13fb7ad16b85e2e9c01eafc73e0d6f6dd75822
primozg123 <primoz.golle@gmail.com>
2024-01-17 20:25:44 +0100

    Mobile #2 & cookies

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,12 @@
+$(document).ready(function () {
+    const cookies = localStorage.getItem('cookies');
+    const cookiesFooter = document.getElementById("cookies");
+    if (cookies) {
+        cookiesFooter.hidden = true;
+    }
+});
 
-
+function confirmCookies() {
+    localStorage.setItem('cookies', 'true');
+    document.getElementById("cookies").hidden = true;
+}
