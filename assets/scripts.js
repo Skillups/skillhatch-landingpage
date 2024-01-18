ab27ec6e3b831c1d6e710bc942f6959bd79b7999
primozg123 <primoz.golle@gmail.com>
2024-01-18 18:37:11 +0100

    Tmp solution

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
     if (cookies) {
         cookiesFooter.hidden = true;
     }
+
+    document.body.addEventListener("click", function (evt) {
+        const mobileheader = document.getElementsByClassName("dropdown-content")[0];
+        const display = window.getComputedStyle(mobileheader).getPropertyValue('display');
+
+
+        if (display == "none"){
+            mobileheader.display = "none"
+            /*console.dir(this);
+            //note evt.target can be a nested element, not the body element, resulting in misfires
+            console.log(evt.target);
+            alert("body clicked");*/
+        }
+    });
 });
 
 function confirmCookies() {
