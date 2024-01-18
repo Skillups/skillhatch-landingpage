5953a8da49b51be5bc89fac5563bdf1da566bd5b
primozg123 <primoz.golle@gmail.com>
2024-01-18 19:03:34 +0100

    Working mobile menu

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,11 +5,9 @@ $(document).ready(function () {
         cookiesFooter.hidden = true;
     }
 
-    document.body.addEventListener("click", function (evt) {
+    document.body.addEventListener("click", function () {
         const mobileheader = document.getElementsByClassName("dropdown-content")[0];
-        const display = window.getComputedStyle(mobileheader).getPropertyValue('display');
-
-        if (display == "none"){
+        if (window.getComputedStyle(mobileheader).getPropertyValue('display') === "none"){
             window.focus();
             if (document.activeElement) {
                 document.activeElement.blur();
@@ -21,8 +19,4 @@ $(document).ready(function () {
 function confirmCookies() {
     localStorage.setItem('cookies', 'true');
     document.getElementById("cookies").hidden = true;
-}
-
-function toggleMenu(){
-    document.getElementsByClassName("dropdown-content")[0].toggle();
 }
